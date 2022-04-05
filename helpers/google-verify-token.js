358a8281391830cc06e105e35a12b72074f0e725
desarrollop <desarrollo.phara@outlook.es>
2022-04-05 09:08:11 -0600

    Google Id Token listo

---
+++ helpers/google-verify-token.js
@@ -0,0 +1,33 @@
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID='56446747319-278ap394h1vp3bs7frvhke39j4bafjvo.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
+
+const validarGoogleIdToken= async(token)=>{
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: [
+                CLIENT_ID,
+                '56446747319-eps4dqto1ak5lnnjdq648npl6hm6gshc.apps.googleusercontent.com'
+            ],  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        //const userid = payload['sub'];
+        //console.log(payload);
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+        return{
+            name: payload['name'],
+            picture: payload['picture'],
+            email: payload['email'],
+        }
+    } catch (error) {
+        return null;
+    }
+}
+
+module.exports={
+    validarGoogleIdToken,
+}
